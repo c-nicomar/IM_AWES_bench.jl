159ef403c97b3d3eb77ff3eb360a97a58b604552
Carolina Nicolás <canicola@ing.uc3m.es>
2026-06-10 17:30:15 +0200

    Improved tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,4 +1,10 @@
+using Pkg
+if Base.active_project() != joinpath(@__DIR__, "Project.toml")
+    Pkg.activate(joinpath(@__DIR__))
+end
+
 using Test
+using IM_AWES_bench
 
 @testset "Basic arithmetic" begin
     @test 1+1 == 2
